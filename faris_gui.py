1864be130a08d2a139675755a9e5f842b618e9ef
Faris Redza <faris.redza1999@gmail.com>
2024-02-26 12:42:36 +0000

    init submenu

--- faris_gui.py
+++ faris_gui.py
@@ -23,40 +23,70 @@ def send_input(input_text: str) -> None:
     cli_process.stdin.write(input_text + '\n')
     cli_process.stdin.flush()
 
-def button_click(option: str) -> None:
-    send_input(option)
-
-def set_option(option) -> None:
-    selected_option.set(option)
-
-def create_menu(menu: "dict[str, str]") -> None:
-    width = max(len(value) for value in menu.values())
-    for key, value in menu.items():
-        button = tk.Button(text=value, command=lambda v=key: button_click(v), width=width, anchor="w", master=menu_frame)
-        button.pack(anchor=tk.W)
-
-def create_rotate_submenu(mode: int, menu: "dict[str, str]") -> None:
-    mode_label = tk.Label(text=f"Selected option: {menu_frame.keys(mode)}", master=submenu_frame)
-    mode_label.pack(anchor=tk.W)
+cli_thread = threading.Thread(target=run_cli)
+cli_thread.start()
 
 window = tk.Tk()
 window.title("Faris' Automated RotatIng Standa (FARIS) Motor")
 window.minsize(400,300)
 
-menu_frame = tk.Frame(master=window)
-menu_frame.pack(side=tk.LEFT, padx=10,pady=10)
+main_menu_frame = tk.Frame(master=window)
 
-submenu_frame = tk.Frame(master=window)
+def main_menu_button_click(option: str) -> None:
+    send_input(option)
+    try:
+        int(option)
+    except:
+        pass
+    else:
+        if int(option) >= 1 and int(option) <= 4:
+            create_rotate_menu(mode=option, menu=faris_cli.menu_dict)
+            show_rotate_menu()
 
-cli_thread = threading.Thread(target=run_cli)
-cli_thread.start()
+def set_option(option) -> None:
+    selected_option.set(option)
 
-label = ttk.Label(text="Select option", master=menu_frame)
-label.pack(anchor=tk.W)
+def create_main_menu(menu: "dict[str, str]") -> None:
+    label = ttk.Label(text="Select option", master=main_menu_frame)
+    label.pack(anchor=tk.W)
+
+    width = max(len(value) for value in menu.values())
+    for key, value in menu.items():
+        button = tk.Button(text=value, command=lambda v=key: main_menu_button_click(v), width=width, anchor="w", master=main_menu_frame)
+        button.pack(anchor=tk.W)
+
+def show_main_menu():
+    main_menu_frame.pack(side=tk.LEFT, padx=10,pady=10)
+    rotate_menu_frame.forget()
+
+
+def clear_frame(frame: tk.Frame) -> None:
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+def rotate_menu_quit_click() -> None:
+    send_input('q')
+    show_main_menu()
+    clear_frame(rotate_menu_frame)
+
+def create_rotate_menu(mode: str, menu: "dict[str, str]") -> None:
+    mode_label = tk.Label(text=f"Selected option: {menu.get(mode)}", master=rotate_menu_frame)
+    mode_label.pack(anchor=tk.W)
+    
+    quit_button = tk.Button(text="Back", command=rotate_menu_quit_click, master=rotate_menu_frame)
+    quit_button.pack(anchor=tk.W)
+
+def show_rotate_menu():
+    rotate_menu_frame.pack(side=tk.LEFT, padx=10,pady=10)
+    main_menu_frame.forget()
+
+rotate_menu_frame = tk.Frame(master=window)
+
+show_main_menu()
 
 selected_option = tk.StringVar()
 
-create_menu(faris_cli.menu_dict)
+create_main_menu(faris_cli.menu_dict)
 
 window.protocol("WM_DELETE_WINDOW", on_closing)
 window.mainloop()
