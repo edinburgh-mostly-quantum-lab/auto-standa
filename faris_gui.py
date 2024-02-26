9c18db5fa45642d97eb809273163cd680ca045f4
Faris Redza <faris.redza1999@gmail.com>
2024-02-26 13:26:02 +0000

    added padding

--- faris_gui.py
+++ faris_gui.py
@@ -47,17 +47,17 @@ def set_option(option) -> None:
     selected_option.set(option)
 
 def create_main_menu(menu: "dict[str, str]") -> None:
-    label = ttk.Label(text="Select option", master=main_menu_frame)
-    label.pack(anchor=tk.W)
+    label = tk.Label(text="Select option", master=main_menu_frame)
+    label.pack(anchor=tk.NW, pady=2)
 
     width = max(len(value) for value in menu.values())
     for key, value in menu.items():
         button = tk.Button(text=value, command=lambda v=key: main_menu_button_click(v), width=width, anchor="w", master=main_menu_frame)
-        button.pack(anchor=tk.W)
+        button.pack(anchor=tk.NW)
 
 def show_main_menu():
-    main_menu_frame.pack(side=tk.LEFT, padx=10,pady=10)
-    rotate_menu_frame.forget()
+    main_menu_frame.grid(row=0, column=0, sticky=tk.W, padx=2, pady=2)
+    rotate_menu_frame.grid_forget()
 
 
 def clear_frame(frame: tk.Frame) -> None:
@@ -71,14 +71,14 @@ def rotate_menu_quit_click() -> None:
 
 def create_rotate_menu(mode: str, menu: "dict[str, str]") -> None:
     mode_label = tk.Label(text=f"Selected option: {menu.get(mode)}", master=rotate_menu_frame)
-    mode_label.pack(anchor=tk.W)
+    mode_label.pack(anchor=tk.NW, pady=2)
     
     quit_button = tk.Button(text="Back", command=rotate_menu_quit_click, master=rotate_menu_frame)
-    quit_button.pack(anchor=tk.W)
+    quit_button.pack(anchor=tk.NW, pady=2)
 
 def show_rotate_menu():
-    rotate_menu_frame.pack(side=tk.LEFT, padx=10,pady=10)
-    main_menu_frame.forget()
+    rotate_menu_frame.grid(row=0, column=0, sticky=tk.NW, padx=2, pady=2)
+    main_menu_frame.grid_forget()
 
 rotate_menu_frame = tk.Frame(master=window)
 
@@ -87,6 +87,14 @@ show_main_menu()
 selected_option = tk.StringVar()
 
 create_main_menu(faris_cli.menu_dict)
+
+status_frame = tk.Frame(master=window)
+status_frame.grid(row=0, column=1, sticky=tk.NW, pady=2)
+
+status_label = tk.Label(text="Status", master=status_frame)
+status_label.grid(row=0, column=0, sticky=tk.NW, pady=2)
+status_text = tk.Text(master=status_frame)
+status_text.grid(row=1, column=0, sticky=tk.NW, pady=2)
 
 window.protocol("WM_DELETE_WINDOW", on_closing)
 window.mainloop()
